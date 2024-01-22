28232850ddf695d572317de310c95e552ceadd97
Sohom Neogi <80317920+KingRyuK69@users.noreply.github.com>
2024-01-22 13:40:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const FCM = require("fcm-node");
 
 const serverKey =
-  "AAAA4bo3LGw:APA91bEp3FVPPn73Tlplcy1q-7Nn6FjAH4WoamDfIR1vNkom3x3ldor41i7hk0lrIcT_NtIVOlK-DXx34SJQnbHVcicscP_ECR5sLfigBgJYSSYNwKClrOgFN7pRcInsoB4Qj1mUuGcX";
+  "SERVER_KEY";
 const deviceToken =
-  "cC67m4nLR-K3mq4ldcfcZA:APA91bGVd3jQvxXRzm7Pc0YcRifSXAF8KHxfzQ1Lz5yzyGuhyhv2XTWAITHYxNHt0H3VbIQBdkxXjjEYrtpAOo2n0EFt3QlheTLyNhJx6lIG_j6bqu2aDrMRUa4dt4D8vaxzzTpYL7WA";
+  "DEVICE_TOKEN";
 
 const fcm = new FCM(serverKey);
 
