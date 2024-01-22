79f86fabcad3ae63348a2dc7e4934bc538377056
KingPuke <154311503+KingPuke@users.noreply.github.com>
2024-01-22 13:45:35 +0530

    env varibale patched

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const FCM = require("fcm-node");
+require("dotenv").config();
 
-const serverKey =
-  "AAAA4bo3LGw:APA91bEp3FVPPn73Tlplcy1q-7Nn6FjAH4WoamDfIR1vNkom3x3ldor41i7hk0lrIcT_NtIVOlK-DXx34SJQnbHVcicscP_ECR5sLfigBgJYSSYNwKClrOgFN7pRcInsoB4Qj1mUuGcX";
-const deviceToken =
-  "cC67m4nLR-K3mq4ldcfcZA:APA91bGVd3jQvxXRzm7Pc0YcRifSXAF8KHxfzQ1Lz5yzyGuhyhv2XTWAITHYxNHt0H3VbIQBdkxXjjEYrtpAOo2n0EFt3QlheTLyNhJx6lIG_j6bqu2aDrMRUa4dt4D8vaxzzTpYL7WA";
+const serverKey = process.env.SERVER_KEY;
+const deviceToken = process.env.DEVICE_TOKEN;
 
 const fcm = new FCM(serverKey);
 
